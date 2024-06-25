3e500af76a0f7939336dfe8eb98d26b4c376abb7
8bit_please <68927773+JustNaimoR@users.noreply.github.com>
2024-06-25 15:33:13 +0300

    commit

--- Module2/EvenOddThreads/src/main/java/org/example/Main.java
+++ Module2/EvenOddThreads/src/main/java/org/example/Main.java
@@ -1,7 +1,60 @@
 package org.example;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
+    private static volatile boolean evenTurn = true;
+
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        Thread evenThread = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i += 2) {
+                    System.out.println(i);
+
+                    try {
+                        notifyAll();
+                        wait();
+                    } catch (InterruptedException ex) {
+                        log.error("exception when interrupting a thread - evenThread");
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        Thread oddThread = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 1; i < 100; i += 2) {
+                    System.out.println(i);
+
+                    try {
+                        notifyAll();
+                        wait();
+                    } catch (InterruptedException ex) {
+                        log.error("exception when interrupting a thread - oddThread");
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        evenThread.start();
+        log.info("even working...");
+        oddThread.start();
+        log.info("odd working...");
+
+        try {
+            evenThread.join();
+            oddThread.join();
+            log.info("work is over");
+        } catch (InterruptedException ex) {
+            log.error("exception when interrupting a thread - main");
+            ex.printStackTrace();
+        }
     }
+
 }
