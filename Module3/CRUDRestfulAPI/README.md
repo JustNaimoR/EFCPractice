b9354cc86ee5e033156f7348850983b8442c152b
8bit_please <68927773+JustNaimoR@users.noreply.github.com>
2024-07-05 10:00:49 +0300

    CRUDRestful api - finals

---
+++ Module3/CRUDRestfulAPI/README.md
@@ -0,0 +1,10 @@
+### Простое CRUD приложение для добавления, чтения, удаления todo задач в БД
+
+Из используемого стека:
+- RESTfull api
+- postgreSQL
+- Flyway для миграции
+- jdbcTemplate
+- unit & integration tests
+- cache layer
+- swagger 
--- Module3/CRUDRestfulAPI/src/main/java/edu/mod3/crud/romario/crudrestfulapi/CrudRestfulApiApplication.java
+++ Module3/CRUDRestfulAPI/src/main/java/edu/mod3/crud/romario/crudrestfulapi/CrudRestfulApiApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 //todo
-// - написать README.md с описанием стека (чтобы выделить jdbcTemplate)
 // - спросить у ментора - использовать ResponseEntity или можно обойтись @ResponseStatus
-// - cache слой для Spring
-// - openapi.yml - как туда влить код для swagger?
 
 @SpringBootApplication
 @EnableCaching
