2aa2c8f831edd9e895e05cacb5447534d944304e
8bit_please <68927773+JustNaimoR@users.noreply.github.com>
2024-09-04 11:20:20 +0300

    module 3 - bugfixes

--- Module3 - Spring_SpringBoot/CRUDRestfulAPI/src/main/java/edu/mod3/crud/romario/crudrestfulapi/CrudRestfulApiApplication.java
+++ Module3 - Spring_SpringBoot/CRUDRestfulAPI/src/main/java/edu/mod3/crud/romario/crudrestfulapi/CrudRestfulApiApplication.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-/*
-todo
- - Как добавить тот самогенерирующийся файл с openAPI swagger чтобы не пришлось открывать все время в браузере
- */
-
 @SpringBootApplication
 @EnableCaching
 public class CrudRestfulApiApplication {
